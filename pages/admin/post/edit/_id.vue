eebe1d0cec023c11ec282976fa326ba75dcdc50b
“najikh2002” <hizbullahnajihan@gmail.com>
2024-05-06 04:30:31 +0700

    fix: code display

--- pages/admin/post/edit/_id.vue
+++ pages/admin/post/edit/_id.vue
@@ -173,13 +173,6 @@ export default {
       // config CKEDITOR
       editorConfig: {
         removePlugin: ["Title"],
-        toolbar: [
-          { name: "basicstyles", items: ["Bold", "Italic", "Underline"] },
-          { name: "styles", items: ["Format"] },
-          { name: "list", items: ["NumberedList", "BulletedList"] },
-          { name: "links", items: ["Link", "Unlink"] },
-          { name: "document", items: ["Source"] },
-        ],
         simpleUpload: {
           uploadUrl:
             "https://pejuangpemrogaman.appdev.my.id/api/web/posts/store-image",
--- pages/post/_slug.vue
+++ pages/post/_slug.vue
@@ -264,11 +264,4 @@ export default {
 };
 </script>
 
-<style>
-code {
-  font-family: "Courier New", Courier, monospace;
-  background-color: #f0f0f0;
-  padding: 2px 4px;
-  border-radius: 4px;
-}
-</style>
+<style></style>
